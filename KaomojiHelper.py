cf00bffece47ca5041aff18db25dd330e0e9ad81
senolem <37041587+senolem@users.noreply.github.com>
2024-04-18 19:50:38 +0200

    set style fusion

--- KaomojiHelper.py
+++ KaomojiHelper.py
@@ -246,6 +246,7 @@ class KaomojiHelper(QWidget):
 
 if __name__ == '__main__':
     app = QApplication([])
+    app.setStyle('fusion')
     kaomojiHelper = KaomojiHelper()
     kaomojiHelper.show()
     kaomojiHelper.center()
